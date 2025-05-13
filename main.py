7868962c6cc0fdb7cd4766615bc4517adc9fba9f
0venToast <joeyevelyn0@gmail.com>
2025-05-13 02:16:08 -0400

    Update version to 2.1.3 and modify download URL in version.json

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if hasattr(sys, '_MEIPASS'):
     os.chdir(sys._MEIPASS)
 
 VERSION_URL = "https://raw.githubusercontent.com/0venToast/Action-Runner/refs/heads/main/version.json"
-version = "2.1.2"
+version = "2.1.3"
 
 def download_new_version(download_url, temp_path):
     try:
