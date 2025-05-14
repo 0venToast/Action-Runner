65cfab44766e676b49373a971f12dd43ffa48e9e
0venToast <joeyevelyn0@gmail.com>
2025-05-14 10:35:33 -0400

    Update version to 2.3.10 and set application icon

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if hasattr(sys, '_MEIPASS'):
     os.chdir(sys._MEIPASS)
 
 VERSION_URL = "https://raw.githubusercontent.com/0venToast/Action-Runner/refs/heads/main/version.json"
-version = "2.3.9"
+version = "2.3.10"
 
 def download_new_version(download_url, temp_path):
     try:
@@ -325,6 +325,7 @@ def on_drag_end(event):
 
 # --- GUI Setup ---
 root = tk.Tk()
+root.iconbitmap("icon.ico")
 root.title("Action-Runner")
 check_for_updates()
 
